8d60f56d4fe8a1621020ca8fb37fb47d32f49082
MaxMorfiX <offmax.me@gmail.com>
2026-05-05 19:39:34 +0200

    decided to start from the other end

---
+++ src/blocks/music.ts
@@ -0,0 +1,33 @@
+/**
+ * @license
+ * Copyright 2023 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import * as Blockly from 'blockly/core';
+import {javascriptGenerator, Order} from 'blockly/javascript';
+// Create a custom block called 'add_text' that adds
+// text to the output div on the sample app.
+// This is just an example and you should replace this with your
+// own custom blocks.
+const getEnergy = {
+  "type": "get_energy",
+  "tooltip": "",
+  "helpUrl": "",
+  "message0": "Volume %1",
+  "args0": [
+    {
+      "type": "input_dummy",
+      "name": "label"
+    }
+  ],
+  "output": Number,
+  "colour": 225
+};
+
+// Create the block definitions for the JSON-only blocks.
+// This does not register their definitions with Blockly.
+// This file has no side effects!
+export const blocks = Blockly.common.createBlockDefinitionsFromJsonArray([
+    getEnergy,
+]);
---
+++ src/generators/javascript.ts
@@ -0,0 +1,42 @@
+/**
+ * @license
+ * Copyright 2023 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { Order } from 'blockly/javascript';
+import * as Blockly from 'blockly/core';
+
+// Export all the code generators for our custom blocks,
+// but don't register them with Blockly yet.
+// This file has no side effects!
+export const forBlock = Object.create(null);
+
+forBlock['add_text'] = function (
+    block: Blockly.Block,
+    generator: Blockly.CodeGenerator,
+) {
+    const text = generator.valueToCode(block, 'TEXT', Order.NONE) || "''";
+    const addText = generator.provideFunction_(
+        'addText',
+        `function ${generator.FUNCTION_NAME_PLACEHOLDER_}(text) {
+
+            // Add text to the output area.
+            const outputDiv = document.getElementById('output');
+            const textEl = document.createElement('p');
+            textEl.innerText = text;
+            outputDiv.appendChild(textEl);
+        }`,
+    );
+    // Generate the function call for this block.
+    const code = `${addText}(${text});\n`;
+    return code;
+};
+
+forBlock['get_energy'] = function() {
+
+  // TODO: Assemble javascript into the code variable.
+  const code = '20';
+  // TODO: Change Order.NONE to the correct operator precedence strength
+  return [code, Order.NONE];
+}
